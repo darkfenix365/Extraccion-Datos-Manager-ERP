1d758b0cb2628101eb0a0cfa73b7c0fac4c0017d
Mauricio Ortiz Bustos <mortizb@fen.uchile.cl>
2022-09-12 10:08:53 -0300

    script ext clientes

--- automatizacion/manager_aut/funciones_manager.py
+++ automatizacion/manager_aut/funciones_manager.py
@@ -21,3 +21,13 @@ def manager_auth(manager_user, manager_pass):
     token_auth = token_manager['auth_token']
     return token_auth
 
+def clientes_manager(token_manager_auth, rut_empresa):
+  url = f"https://lemontech.managermas.cl/api/clients/{rut_empresa}/?contacts=1&direcciones=1"
+
+  payload={}
+  headers = {
+    'Authorization': f'Token {token_manager_auth}'
+  }
+  response = rq.request("GET", url, headers=headers, data=payload)
+
+  print(response.text)
